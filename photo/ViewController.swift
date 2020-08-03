6e34de6cbeab7f64767e88b9c003af5ded0560c5
s4226 <s4226@kimo.com>
2020-08-03 09:47:57 +0800

    Initial Commit

---
+++ photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo
+//
+//  Created by hbe11678 on 2020/8/3.
+//  Copyright © 2020 hbe11678. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
